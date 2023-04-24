01b3bb73da7d287ced34dce2665a1059a4c3e6e7
peteredwards <p.l.edwards@leeds.ac.uk>
2023-04-24 15:19:34 +0100

    Added pages for each space

---
+++ _scripts/generate-spaces.js
@@ -0,0 +1,20 @@
+/**
+ * This script takes the spaces data file and creates pages in the spaces directory
+ */
+const fs = require('fs');
+const path = require('path');
+const YAML = require('json-to-pretty-yaml');
+
+/* load space data */
+var spacedata = fs.readFileSync( path.resolve( __dirname, '../_data/spaces.json' ), { encoding: 'utf8' } );
+var spaceJSON = JSON.parse(spacedata);
+/* create pages for each space with all data in frontmatter */
+spaceJSON.forEach( space => {
+    var itemYAML = "---\nlayout: space\npermalink: /" + space.slug + "/\n" + YAML.stringify( space ) + "\n---\n";
+    fs.writeFile( path.resolve( __dirname, '../pages/', space.slug + '.md' ), itemYAML, err => {
+        if (err) {
+            console.error( err );
+            return;
+        }
+    });
+});
